8c6bcfc5a7115f5758c843d4cb5f9ffc810722c3
Andrey Subbotin <a.subbotin@haulmont.com>
2023-12-17 18:48:04 +0400

    User Tasks from Camunda addon

--- src/main/java/com/example/insurancedemo/InsuranceDemoApplication.java
+++ src/main/java/com/example/insurancedemo/InsuranceDemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.insurancedemo;
 
-import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-//@EnableZeebeClient
-//@Deployment(resources = "classpath*:/bpmn/**/*.bpmn")
+@Deployment(resources = "classpath*:/bpmn/**/*.bpmn")
 public class InsuranceDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(InsuranceDemoApplication.class, args);
